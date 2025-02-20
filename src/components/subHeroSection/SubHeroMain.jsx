8c640bab10b8117809d38cd0b46650a20eef5162
tahmid <tahmid.ae@gmail.com>
2025-02-20 13:29:27 +0600

    20 130pm

--- src/components/subHeroSection/SubHeroMain.jsx
+++ src/components/subHeroSection/SubHeroMain.jsx
@@ -1,9 +1,9 @@
 const SubHeroMain = () => {
   return (
     <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 md:flex-row sm:flex-col items-center gap-4">
-      <p className="md:block sm:hidden"> Indulge in</p>
-      <p className="md:block sm:hidden">luxury</p>
-      <p>with care</p>
+      <p className="md:block sm:hidden"> Code</p>
+      <p className="md:block sm:hidden">Innovate</p>
+      <p>Transform</p>
     </div>
   );
 };
