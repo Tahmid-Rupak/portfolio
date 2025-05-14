9f295a414a63cbbd21dae2141a241e21d8a247b3
tahmid <tahmid.ae@gmail.com>
2025-05-14 14:08:31 +0600

    Commit 14525

--- src/components/contactMeSection/ContactInfo.jsx
+++ src/components/contactMeSection/ContactInfo.jsx
@@ -6,7 +6,7 @@ import SingleInfo from "./SingleInfo";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-2">
-      <SingleInfo text="info@innovationait.com" Image={HiOutlineMail} />
+      <SingleInfo text="isprike36.1@gmail.com" Image={HiOutlineMail} />
       <SingleInfo text="+88 01518907202" Image={FiPhone} />
       <SingleInfo text="Dinajpur, Bangladesh" Image={IoLocationOutline} />
     </div>
--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,7 +1,7 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">Innovation -AIT</h1>
+      <h1 className="text-white  text-2xl sm:hidden md:block">Falah -Lab</h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
         IAIT
       </h1>
